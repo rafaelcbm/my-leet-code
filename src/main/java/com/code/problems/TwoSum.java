ff429c7fb34853c6095cea22ec8af9fc5e9fbbb5
Rafael Correia <rafaelcbm@gmail.com>
2025-04-07 16:44:14 -0300

    first exercise Two Sum

---
+++ src/main/java/com/code/problems/TwoSum.java
@@ -0,0 +1,73 @@
+package com.code.problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    // Approach 3: One-pass Hash Table
+    public int[] twoSum_3(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+
+    // Approach 2: Two-pass Hash Table
+    public int[] twoSum_2(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+
+
+        return new int[]{};
+    }
+
+
+    //Approach 1: Brute Force
+    public int[] twoSum_1(int[] nums, int target) {
+
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 17;
+        int[] result = twoSum.twoSum_3(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
